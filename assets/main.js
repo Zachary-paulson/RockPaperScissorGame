78d5e00ebc6ac9b646a006eaa9ae4506edb71927
Zachary Paulson <zachary.paulson@outlook.com>
2019-03-15 21:40:02 -0700

    Working on game logic

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,40 @@
-$(document).ready(function(){
-    console.log("testing");
+$(document).ready(function () {
+
+    let game = {
+        playerOneAtk: "",
+        playerTwoAtk: "",
+        gameState: ["win", "lose", "draw"],
+        scoreBoard: {
+            wins: 0,
+            losses: 0,
+            draws: 0
+        }
+    };
+
+
+
+    // button listener
+    $(document).on("click", "button", function () {
+
+        game.playerOneAtk = $(this).val();
+        game.playerTwoAtk = "scissors";
+        checkMatchWinner(game.playerOneAtk, game.playerTwoAtk);
+    });
+
+
+
+
 });
+
+function checkMatchWinner(plyrOneAtk, plyrTwoAtk) {
+    if (plyrOneAtk === "rock" && plyrTwoAtk === "scissors" || plyrOneAtk === "paper" && plyrTwoAtk === "rock" || plyrOneAtk === "scissors" && plyrTwoAtk === "paper") {
+        console.log("Winner!");
+    }
+    else if (plyrOneAtk === "rock" && plyrTwoAtk === "paper" || plyrOneAtk === "paper" && plyrTwoAtk === "scissors" || plyrOneAtk === "scissors" && plyrTwoAtk === "rock") {
+        console.log("Loser!");
+    }
+    else {
+        console.log("Draw!");
+    }
+
+};
