4462e0f130f42c7f44228c9ae78b4d6c745d4a57
Zachary Paulson <zachary.paulson@outlook.com>
2019-03-16 09:25:03 -0700

    added database and verified that it functions

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,47 @@
 $(document).ready(function () {
+    //firebase
+    // Initialize Firebase
+  let config = {
+    apiKey: "AIzaSyBk_mX8JY8rY2Z5YbQe-hqDkJbJJP_KoFE",
+    authDomain: "rpsbattlearenazjp.firebaseapp.com",
+    databaseURL: "https://rpsbattlearenazjp.firebaseio.com",
+    projectId: "rpsbattlearenazjp",
+    storageBucket: "",
+    messagingSenderId: "685148480823"
+  };
+  firebase.initializeApp(config);
+
+    // game object
+    let player = {
+        playerAtk: "",
+             
+    };
 
     let game = {
-        playerOneAtk: "",
-        playerTwoAtk: "",
         gameState: ["win", "lose", "draw"],
         scoreBoard: {
             wins: 0,
             losses: 0,
             draws: 0
         }
+
     };
+   //my database
+    const database = firebase.database();
 
-
-
-    // button listener
-    $(document).on("click", "button", function () {
-
-        game.playerOneAtk = $(this).val();
+    // button listener RPS buttons
+    $(".btn.plyrAtkBtn").on("click", function () {
+        player.playerAtk = $(this).val();
+        database.ref().push(player);
         game.playerTwoAtk = "scissors";
-        checkMatchWinner(game.playerOneAtk, game.playerTwoAtk);
+
+        //checkMatchWinner(game.playerOneAtk, game.playerTwoAtk);
+    });
+
+    database.ref().on("child_added", function(childsnapshot){
+        let newEle = $("<h2>");
+        newEle.text("Player Attacked with " + childsnapshot.val().playerAtk);
+        $(".playerOne").append(newEle);
     });
 
 
